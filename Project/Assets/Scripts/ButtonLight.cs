821c7ab4a414fdd05e86a3f5710c30e6fec2c00b
Stjerndal <stjernd@kth.se>
2014-11-27 15:04:42 +0100

    Created EnableByButton script, tested on level2.

--- Project/Assets/Scripts/ButtonLight.cs
+++ Project/Assets/Scripts/ButtonLight.cs
@@ -6,7 +6,7 @@ public class ButtonLight : MonoBehaviour, IButtonListener {
 	public Light myLight;
 
 	public void onButtonPressed() {
-		myLight.intensity = 0.75f;
+		myLight.intensity = 8f;
 	}
 
 	public void onButtonReleased() {
---
+++ Project/Assets/Scripts/EnableByButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableByButton : MonoBehaviour, IButtonListener  {
+
+	public void onButtonPressed() {
+		gameObject.SetActive(true);
+	}
+	
+	public void onButtonReleased() {
+		gameObject.SetActive(false);
+	}
+}
