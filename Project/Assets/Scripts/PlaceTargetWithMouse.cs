fb05b348dfff58e4ba5cc7e143bbdc089bd34046
Mattias Lönnerberg <mattias.lonnerberg@gmail.com>
2014-11-27 12:51:17 +0100

    Can now remove magnets
    also, bug fixed the unpolar layer

--- Project/Assets/Scripts/PlaceTargetWithMouse.cs
+++ Project/Assets/Scripts/PlaceTargetWithMouse.cs
@@ -20,7 +20,8 @@ public class PlaceTargetWithMouse : MonoBehaviour
 			placeMagnet(false);
 		else if (Input.GetMouseButtonDown(1))
 			placeMagnet(true);
-	    
+	    else if (Input.GetMouseButtonDown(2))
+			neutralizeMagnet();
 	}
 
 	void placeMagnet(bool attract) {
@@ -30,10 +31,11 @@ public class PlaceTargetWithMouse : MonoBehaviour
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		RaycastHit hit;
 		if (!Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))return; // if we hit nothing, or the player
+		else if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Unpolar")) return; // if we hit unpolar
 		//            transform.position = hit.point + hit.normal*surfaceOffset;
 
-		layerMask = 1 << unpolarLayer; //hit only the unpolar layer
-		if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))return; // if we hit unpolar
+		//layerMask = 1 << unpolarLayer; //hit only the unpolar layer
+		//if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))return; // if we hit unpolar
 
 		layerMask = 1 << magnetLayer; //hit only the magnet layer
 //		layerMask = ~layerMask; //inverse
@@ -48,6 +50,27 @@ public class PlaceTargetWithMouse : MonoBehaviour
 			Instantiate(attract ? magnetAttracting : magnetRepelling, hit.point + hit.normal*surfaceOffset, transform.rotation);
 		}
 
+	}
+
+	void neutralizeMagnet() {
+		int layerMask = 1 << playerLayer; //hit only the player layer
+		layerMask = ~layerMask; //inverse
+		
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (!Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))return; // if we hit nothing, or the player
+		else if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Unpolar")) return; // if we hit unpolar
+		//            transform.position = hit.point + hit.normal*surfaceOffset;
+		
+		//layerMask = 1 << unpolarLayer; //hit only the unpolar layer
+		//if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))return; // if we hit unpolar
+		
+		layerMask = 1 << magnetLayer; //hit only the magnet layer
+		//		layerMask = ~layerMask; //inverse
+		if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask)) {
+			Destroy(hit.transform.gameObject);
+			return;
+		}
 	}
 }
 
