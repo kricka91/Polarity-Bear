9362ae4045e9cc8978d503c2ed7bd36754c09e89
Daniel Molin <dmol@kth.se>
2014-12-01 16:57:22 +0100

    Add new Goal prefab

---
+++ Project/Assets/Scripts/Goal.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+
+	IGoalListener[] myListeners;
+	public GameObject[] listeners;
+	
+	void Awake() {
+		myListeners = new IGoalListener[listeners.Length];
+		for (int i = 0; i < listeners.Length; ++i) {
+				myListeners [i] = (IGoalListener)listeners[i].GetComponent(typeof(IGoalListener));
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.name == "First Person Controller") {
+			foreach(IGoalListener listener in myListeners)
+				listener.onPlayerEnter();
+		}
+	}
+}
