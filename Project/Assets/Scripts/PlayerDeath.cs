35fefa24a48bfd2a758d156fc3fde3281241dc86
Daniel Molin <dmol@kth.se>
2014-12-02 14:57:10 +0100

    World fades out as player falls to death
    :skull: :skull: :skull: :skull: :skull: :skull: :skull: :skull:

--- Project/Assets/Scripts/PlayerDeath.cs
+++ Project/Assets/Scripts/PlayerDeath.cs
@@ -7,14 +7,28 @@ using System.Collections;
 public class PlayerDeath : MonoBehaviour {
 	private GameObject player;
 
+	 /* At which point the player should be considered out of the map (starts falling to its death */
+	public float fallStart;
+	/* At which point the fall ends and the player dies */
+	public float fallEnd;
+	/* Start color of ambient light */
+	private Color startColor;
+
 	// Use this for initialization
 	void Start () {
 		player = (GameObject.Find("First Person Controller"));
+		startColor = RenderSettings.ambientLight;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(player.transform.position.y < -20){
+		// Make world fade to black as player falls //TODO: Make whole scren black?
+		if(player.transform.position.y < fallStart){
+			float c = Mathf.Abs((player.transform.position.y-fallEnd)/(fallEnd-fallStart));
+			RenderSettings.ambientLight = new Color(c*startColor.r,c*startColor.g,c*startColor.b);
+		}
+		
+		if(player.transform.position.y < fallEnd){
 			Debug.Log("Player has fallen into the void");
 			Application.LoadLevel (Application.loadedLevelName);
 		}
