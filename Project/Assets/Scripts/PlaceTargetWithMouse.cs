124ae1a607edb2dcb2b209f69d652264cc517772
Mattias Lönnerberg <mattias.lonnerberg@gmail.com>
2014-12-10 16:33:45 +0100

    Test with a different kind of level
    Other changes:
    Added a mode for the magnet gun where you hold down a mouse button to
    keep the magnet active. With this it is only possible to have one magnet
    of each at any given time.
    Changed so that magnets don't have collision

--- Project/Assets/Scripts/PlaceTargetWithMouse.cs
+++ Project/Assets/Scripts/PlaceTargetWithMouse.cs
@@ -11,11 +11,33 @@ public class PlaceTargetWithMouse : MonoBehaviour
 	public const int unpolarLayer = 8;
 	public const int magnetLayer = 9;
 	public const int playerLayer = 10;
+	public bool holdDownButtons = false;
+	private Transform attractMagnet;
+	private Transform repelMagnet;
 		
+	private void Start() {
+		if (holdDownButtons) {
+			attractMagnet = (Transform)Instantiate(magnetAttracting, Vector3.zero, Quaternion.identity);
+			repelMagnet =  (Transform)Instantiate(magnetRepelling, Vector3.zero, Quaternion.identity);
 
+			attractMagnet.gameObject.SetActive(false);
+			repelMagnet.gameObject.SetActive(false);
+		}
+	}
 	// Update is called once per frame
 	private void Update()
 	{
+		if (holdDownButtons) {
+			if (Input.GetMouseButtonDown(0))
+				activateMagnet(repelMagnet);
+			if (Input.GetMouseButtonDown(1))
+				activateMagnet(attractMagnet);
+			if (Input.GetMouseButtonUp(0))
+				deactivateMagnet(repelMagnet);
+			if (Input.GetMouseButtonUp(1))
+				deactivateMagnet(attractMagnet);
+			return;
+		}
 	    if (Input.GetMouseButtonDown(0))
 			placeMagnet(false);
 		else if (Input.GetMouseButtonDown(1))
@@ -73,6 +95,36 @@ public class PlaceTargetWithMouse : MonoBehaviour
 			if (character != null) character.setAffectedByPolarity(false);
 			return;
 		}
+	}
+
+	private void activateMagnet(Transform magnet) {
+		
+		int layerMask = 1 << playerLayer; //hit only the player layer
+		layerMask = ~layerMask; //inverse
+		
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (!Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))return; // if we hit nothing, or the player
+		else if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Unpolar")) return; // if we hit unpolar
+
+		layerMask = 1 << magnetLayer; //hit only the magnet layer
+		//		layerMask = ~layerMask; //inverse
+		if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask)) {
+			return;
+		}
+		else {
+			
+			magnet.gameObject.SetActive(true);
+			Vector3 targetPosition = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+			magnet.position = targetPosition;
+			return;
+		}
+	}
+
+	private void deactivateMagnet(Transform magnet) {
+		magnet.gameObject.SetActive (false);
+		CharacterControls character = gameObject.GetComponent<CharacterControls>();
+		if (character != null) character.setAffectedByPolarity(false);
 	}
 }
 
