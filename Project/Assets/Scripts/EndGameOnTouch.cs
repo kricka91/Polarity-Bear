f2971569ef9a3f8bc5aedaff0c3c7d791970cee8
Mattias Lönnerberg <mattias.lonnerberg@gmail.com>
2014-12-14 14:30:25 +0100

    End game easier level

---
+++ Project/Assets/Scripts/EndGameOnTouch.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndGameOnTouch : MonoBehaviour {
+
+	// Use this for initialization
+	void OnCollisionEnter(Collision collision) {
+		// layer #10 is the player layer
+		if (collision.collider.gameObject.layer == 10) {
+			GameManager.GameOver();
+		}
+	}
+}
