a36d0784537e297ce8234c7a3344081bf431505d
Mattias Lönnerberg <mattias.lonnerberg@gmail.com>
2014-12-14 10:44:55 +0100

    Added all changes from differentLevelBranch

---
+++ Project/Assets/Scripts/ButtonMagnet.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Magnet))]
+public class ButtonMagnet : MonoBehaviour, IButtonListener {
+
+	private Magnet magnetComponent;
+
+	// Use this for initialization
+	void Start () {
+		magnetComponent = gameObject.GetComponent<Magnet> ();
+		magnetComponent.enabled = false;
+	}
+	
+	public void onButtonPressed() {
+		magnetComponent.enabled = true;
+	}
+	
+	public void onButtonReleased() {
+		magnetComponent.enabled = false;
+	}
+}
